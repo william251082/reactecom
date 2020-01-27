42edeb6747df3b4e3bb8b1ecd450b15720f6d1ff
william <william.del.rosario@iceshop.nl>
2020-01-27 17:04:45 +0100

    Menu and directory components -- Going through our folder structure for the project, breaking out homepage component into smaller components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import HomePage from "./homepage.component";
 
-class App extends Component {
-  render() {
-    return <div className='App'><HomePage/></div>;
-  }
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
